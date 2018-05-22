5a2ebb186ad2040a17ae440358b77b6c9b34fca1
Walter Santillan <walter_santillan@hotmail.com>
2018-05-22 16:32:09 -0300

    Commit work

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,6 +1,4 @@
-import React, {
-    Component
-} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
